7288b1ba168c47584dfb1e19a56170aa7e30ffde
Johann Schopplich <mail@johannschopplich.com>
2021-11-17 18:45:23 +0100

    chore: formatting

--- scripts/release.js
+++ scripts/release.js
@@ -3,23 +3,23 @@
 /*
  * Modified from https://github.com/vuejs/vue-next/blob/master/scripts/release.js
  */
-const fs = require('fs')
-const path = require('path')
-const semver = require('semver')
-const prompts = require('prompts')
-const execa = require('execa')
-const { cyan } = require('colorette')
-const { version: currentVersion } = require('../package.json')
+const fs = require("fs");
+const path = require("path");
+const semver = require("semver");
+const prompts = require("prompts");
+const execa = require("execa");
+const { cyan } = require("colorette");
+const { version: currentVersion } = require("../package.json");
 
 /**
  * @type {import('semver').ReleaseType[]}
  */
-const versionIncrements = ['patch', 'minor', 'major']
+const versionIncrements = ["patch", "minor", "major"];
 
 /**
  * @param {import('semver').ReleaseType} i
  */
-const inc = (i) => semver.inc(currentVersion, i)
+const inc = (i) => semver.inc(currentVersion, i);
 
 /**
  * @param {string} bin
@@ -27,104 +27,104 @@ const inc = (i) => semver.inc(currentVersion, i)
  * @param {object} opts
  */
 const run = (bin, args, opts = {}) =>
-  execa(bin, args, { stdio: 'inherit', ...opts })
+  execa(bin, args, { stdio: "inherit", ...opts });
 
 /**
  * @param {string} msg
  */
-const step = (msg) => console.log(cyan(msg))
+const step = (msg) => console.log(cyan(msg));
 
 async function main() {
-  let targetVersion
+  let targetVersion;
 
   /**
    * @type {{ release: string }}
    */
   const { release } = await prompts({
-    type: 'select',
-    name: 'release',
-    message: 'Select release type',
+    type: "select",
+    name: "release",
+    message: "Select release type",
     choices: versionIncrements
       .map((i) => `${i} (${inc(i)})`)
-      .concat(['custom'])
-      .map((i) => ({ value: i, title: i }))
-  })
+      .concat(["custom"])
+      .map((i) => ({ value: i, title: i })),
+  });
 
-  if (release === 'custom') {
+  if (release === "custom") {
     /**
      * @type {{ version: string }}
      */
     const res = await prompts({
-      type: 'text',
-      name: 'version',
-      message: 'Input custom version',
-      initial: currentVersion
-    })
-    targetVersion = res.version
+      type: "text",
+      name: "version",
+      message: "Input custom version",
+      initial: currentVersion,
+    });
+    targetVersion = res.version;
   } else {
-    targetVersion = release.match(/\((.*)\)/)[1]
+    targetVersion = release.match(/\((.*)\)/)[1];
   }
 
   if (!semver.valid(targetVersion)) {
-    throw new Error(`Invalid target version: ${targetVersion}`)
+    throw new Error(`Invalid target version: ${targetVersion}`);
   }
 
   const { yes: tagOk } = await prompts({
-    type: 'confirm',
-    name: 'yes',
-    message: `Releasing v${targetVersion}. Confirm?`
-  })
+    type: "confirm",
+    name: "yes",
+    message: `Releasing v${targetVersion}. Confirm?`,
+  });
 
   if (!tagOk) {
-    return
+    return;
   }
 
   // Update the package version.
-  step('\nUpdating the package version...')
-  updatePackage(targetVersion)
+  step("\nUpdating the package version...");
+  updatePackage(targetVersion);
 
   // Build the package.
-  step('\nBuilding the package...')
-  await run('pnpm', ['run', 'build'])
+  step("\nBuilding the package...");
+  await run("pnpm", ["run", "build"]);
 
   // Generate the changelog.
-  step('\nGenerating the changelog...')
-  await run('pnpm', ['run', 'changelog'])
-  await run('npx', ['prettier', '--write', 'CHANGELOG.md'])
+  step("\nGenerating the changelog...");
+  await run("pnpm", ["run", "changelog"]);
+  await run("npx", ["prettier", "--write", "CHANGELOG.md"]);
 
   const { yes: changelogOk } = await prompts({
-    type: 'confirm',
-    name: 'yes',
-    message: `Changelog generated. Does it look good?`
-  })
+    type: "confirm",
+    name: "yes",
+    message: `Changelog generated. Does it look good?`,
+  });
 
   if (!changelogOk) {
-    return
+    return;
   }
 
   // Commit changes to the Git and create a tag.
-  step('\nCommitting changes...')
-  await run('git', ['add', '*'])
-  await run('git', ['commit', '-m', `release: v${targetVersion}`])
-  await run('git', ['tag', `v${targetVersion}`])
+  step("\nCommitting changes...");
+  await run("git", ["add", "*"]);
+  await run("git", ["commit", "-m", `release: v${targetVersion}`]);
+  await run("git", ["tag", `v${targetVersion}`]);
 
   // Publish the package.
-  step('\nPublishing the package...')
-  await run('npm', ['publish', '--no-commit-hooks', '--no-git-tag-version'])
+  step("\nPublishing the package...");
+  await run("npm", ["publish", "--no-commit-hooks", "--no-git-tag-version"]);
 
   // Push to GitHub.
-  step('\nPushing to GitHub...')
-  await run('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
-  await run('git', ['push'])
+  step("\nPushing to GitHub...");
+  await run("git", ["push", "origin", `refs/tags/v${targetVersion}`]);
+  await run("git", ["push"]);
 }
 
 function updatePackage(version) {
-  const pkgPath = path.resolve(__dirname, '..', 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  const pkgPath = path.resolve(__dirname, "..", "package.json");
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
 
-  pkg.version = version
+  pkg.version = version;
 
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
 }
 
-main().catch((err) => console.error(err))
+main().catch((err) => console.error(err));
