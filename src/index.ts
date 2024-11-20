14bc402a9c8c2059a53b70ba0d3341d6c5900e20
Johann Schopplich <mail@johannschopplich.com>
2024-11-20 15:03:59 +0100

    refactor: outsource constant defaults

--- src/index.ts
+++ src/index.ts
@@ -26,24 +26,29 @@ export interface AnimereOptions {
 
 export { animate, isCrawler, prefersReducedMotion }
 
+const DEFAULT_PREFIX = 'animere'
+const DEFAULT_OFFSET = 0.2
+const ANIMATE_PREFIX = 'animate__'
+
 /**
  * CSS-driven scroll-based animations
  */
 export function createAnimere({
-  prefix = 'animere',
+  prefix = DEFAULT_PREFIX,
   offset,
   shouldInitialize,
 }: AnimereOptions = {}) {
-  const _prefix = toKebabCase(prefix)
+  const kebabCasePrefix = toKebabCase(prefix)
+  const canInitialize = shouldInitialize?.() ?? (!prefersReducedMotion && !isCrawler)
 
   // Skip initialization if the user prefers a reduced amount
   // of motion or a crawler visits the website
-  if (shouldInitialize?.() ?? (!prefersReducedMotion && !isCrawler)) {
+  if (canInitialize) {
     for (const element of document.querySelectorAll<HTMLElement>(
-      `[data-${_prefix}]:not([data-${_prefix}-skip])`,
+      `[data-${kebabCasePrefix}]:not([data-${kebabCasePrefix}-skip])`,
     )) {
       initIntersectionObserver(element, {
-        prefix: _prefix,
+        prefix: kebabCasePrefix,
         offset,
       })
     }
@@ -55,9 +60,9 @@ export function createAnimere({
  */
 function initIntersectionObserver(
   element: HTMLElement,
-  { prefix = 'animere', offset = 0.2 }: AnimereOptions,
+  { prefix = DEFAULT_PREFIX, offset = DEFAULT_OFFSET }: AnimereOptions,
 ) {
-  const _prefix = toCamelCase(prefix)
+  const camelCasePrefix = toCamelCase(prefix)
 
   // Hide element
   element.style.visibility = 'hidden'
@@ -70,10 +75,10 @@ function initIntersectionObserver(
 
     // Add custom properties for `Animate.css` animations from `data`
     // attributes if available, e.g. `data-animere-duration="2s"`
-    Object.keys(element.dataset)
-      .filter(i => i !== _prefix && i.startsWith(_prefix))
-      .forEach((dataAttr) => {
-        const animateOption = dataAttr.slice(_prefix.length).toLowerCase()
+    Object.entries(element.dataset)
+      .filter(([key]) => key !== camelCasePrefix && key.startsWith(camelCasePrefix))
+      .forEach(([dataAttr, value]) => {
+        const animateOption = dataAttr.slice(camelCasePrefix.length).toLowerCase()
         const propertyName = `--animate-${animateOption}`
 
         if (animateOption === 'delay')
@@ -81,7 +86,7 @@ function initIntersectionObserver(
         if (animateOption === 'repeat')
           element.style.animationIterationCount = `var(${propertyName})`
 
-        element.style.setProperty(propertyName, element.dataset[dataAttr]!)
+        element.style.setProperty(propertyName, value!)
       })
 
     // Show element
@@ -91,10 +96,10 @@ function initIntersectionObserver(
     observer.unobserve(element)
 
     // Start animation and wait for it to finish
-    await animate(element, element.dataset[_prefix]!, 'animate__')
+    await animate(element, element.dataset[camelCasePrefix]!, ANIMATE_PREFIX)
 
     // Mark element as animated
-    element.dataset[`${_prefix}Finished`] = 'true'
+    element.dataset[`${camelCasePrefix}Finished`] = 'true'
   }
 
   const observer = new IntersectionObserver(callback, { threshold: offset })
